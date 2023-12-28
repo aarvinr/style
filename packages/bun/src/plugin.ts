ac5943045bcde5a0971b81a21763d8a125643eba
Aarvin R. <144612896+aarvinr@users.noreply.github.com>
2023-12-28 17:32:23 +0000

    bun: create bun plugin - update plugin.ts

--- packages/bun/src/plugin.ts
+++ packages/bun/src/plugin.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import { type BunPlugin } from "bun";
+
+import { readFileSync } from "fs";
+import { compile } from "@stylecss/compiler";
+
+const stylePlugin: BunPlugin = {
+  name: "Style",
+  setup(build) {
+    build.onLoad({ filter: /\.style$/ }, ({ path }) => {
+      const response = readFileSync(path, "utf-8");
+
+      return {
+        exports: { default: compile(response) },
+        loader: "object",
+      };
+    });
+  },
+};
