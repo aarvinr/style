9cd148804ab3e2cc871b794b60d4e8363feccd35
Aarvin R. <144612896+aarvinr@users.noreply.github.com>
2023-12-28 17:56:43 +0000

    plugin: extract to function - export plugin as function, not variable

--- packages/bun/src/plugin.ts
+++ packages/bun/src/plugin.ts
@@ -3,16 +3,20 @@ import { type BunPlugin } from "bun";
 import { readFileSync } from "fs";
 import { compile } from "@stylecss/compiler";
 
-const stylePlugin: BunPlugin = {
-  name: "Style",
-  setup(build) {
-    build.onLoad({ filter: /\.style$/ }, ({ path }) => {
-      const response = readFileSync(path, "utf-8");
+export function style() {
+  const stylePlugin: BunPlugin = {
+    name: "Style",
+    setup(build) {
+      build.onLoad({ filter: /\.style$/ }, ({ path }) => {
+        const response = readFileSync(path, "utf-8");
 
-      return {
-        exports: { default: compile(response) },
-        loader: "object",
-      };
-    });
-  },
-};
+        return {
+          exports: { default: compile(response) },
+          loader: "object",
+        };
+      });
+    },
+  };
+
+  return stylePlugin;
+}
